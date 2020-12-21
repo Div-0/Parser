276ee3b1ca381595b72b825b6bf47d65d277daeb
Dividi0 <koteyka269@gmail.com>
2020-12-21 22:59:00 +0300

    hello covid last version2

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import pymorphy2
 import nltk
 from nltk.corpus import stopwords
 import threading
+from queue import Queue
+queue = Queue()
 nltk.download('stopwords')
 en_stops = set(stopwords.words('english'))
 morph = pymorphy2.MorphAnalyzer()
@@ -22,179 +24,188 @@ Page_quantity = []
 Page_id = [1]
 Word_id = [1]
 l_allowed = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghigklmnopqrstuvwxyz'
+Thread_list = []
+
+
+def pages_append(url, title, description):
+    Pages.append({
+        'id': Page_id[0],
+        'url': url,
+        'title': title,
+        'description': description,
+    })
+    Page_id.insert(0, Page_id[0] + 1)
+
+
+def quantity_append(word, page, domain):
+    flag = 0
+    for d_word in Domain_quantity:
+        if word == d_word['word']:
+            d_word['quantity'] += 1
+            flag = 1
+            break
+    if flag == 0:
+        Domain_quantity.append({
+            'page id': domain,
+            'word': word,
+            'quantity': 1,
+        })
+    flag = 0
+    for p_word in Page_quantity:
+        if page['url'] == p_word['page id'] and word == p_word['word']:
+            p_word['quantity'] += 1
+            flag = 1
+    if flag == 0:
+        Page_quantity.append({
+            'page id': page['url'],
+            'word': word,
+            'quantity': 1,
+        })
+
+
+def words_append(word, page, domain):
+    flag = 1
+    for letter in word:
+        if letter not in l_allowed:
+            flag = 0
+            break
+    if flag == 1:
+        word = morph.parse(word)[0].normal_form
+        if word not in en_stops:
+            Words.append({
+                'id': Word_id[0],
+                'word': word,
+            })
+        quantity_append(word, page, domain)
+    Word_id.insert(0, Word_id[0] + 1)
+    return flag
+
+
+def page_word_append(page_id, word_id):
+    Page_Word.append({
+        'page id': page_id,
+        'word id': word_id,
+    })
+
+
+def link_word_page_append(page_id, word_id):
+    Link_Word_Page.append({
+        'page id': page_id,
+        'word id': word_id,
+    })
+
+
+def thread_start(target, args):
+    t = threading.Thread(target=target, args=args)
+    Thread_list.append(t)
+    t.start()
+
+
+def thread_limit():
+    for t in Thread_list:
+        t.join()
+    Thread_list.clear()
 
 
 def get_html(url):
     try:
         request = requests.get(url, headers=HEADERS)
-        return request
+        soup = BeautifulSoup(request.text, 'html.parser')
+        return soup
     except:
         return None
 
 
-def add_all_links_recursive(url, host, domain, max_depth=0):
-    # список ссылок, от которых в конце мы рекурсивно запустимся
-    links_to_handle_recursive = []
+def absolute_link(link, host, domain):
+    if all(not link.startswith(prefix) for prefix in FORBIDDEN_PREFIXES):
+        # проверяем, является ли ссылка относительной
+        if link.startswith('/') and not link.startswith('//'):
+            # преобразуем относительную ссылку в абсолютную
+            link = domain + link
+        if host not in link:
+            link = host + link
+    return link
 
-    # получаем html код страницы
-    request = get_html(url)
-    # парсим его с помощью BeautifulSoup
-    if request:
-        try:
-            soup = BeautifulSoup(request.text, 'html.parser')
-            # рассматриваем все теги <a>
-            for tag_a in soup.find_all('a', href=True):
-                # получаем ссылку, соответствующую тегу
-                link = tag_a.get('href')
-                # если ссылка не начинается с одного из запрещённых префиксов
-                if all(not link.startswith(prefix) for prefix in FORBIDDEN_PREFIXES):
-                    # проверяем, является ли ссылка относительной
-                    if link.startswith('/') and not link.startswith('//'):
-                        # преобразуем относительную ссылку в абсолютную
-                        link = domain + link
-                    if host not in link:
-                        link = host + link
-                    # проверяем, что ссылка ведёт на нужный домен
-                    # и что мы ещё не обрабатывали такую ссылку
-                    if urlparse(link).netloc == domain:
-                        flag = 0
-                        for page in Pages:
-                            if page['url'] == link:
-                                flag = 1
-                                break
-                        if flag == 0:
-                            Pages.append({
-                                'id': Page_id[0],
-                                'url': link,
-                                'title': 'No title',
-                                'description': 'No description',
-                            })
-                            Page_id.insert(0, Page_id[0] + 1)
-                            links_to_handle_recursive.append(link)
-        except:
-            pass
 
-    if max_depth == 0:
-        return
-    for link in links_to_handle_recursive:
-        add_all_links_recursive(link, host, domain)
+def check_link_to_replay(link):
+    flag = 1
+    for page in Pages:
+        if page['url'] == link:
+            flag = 0
+            break
+    return flag
+
+
+def add_all_links_recursive(url, host, domain, threads_count=1, max_depth=0):
+    soup = get_html(url)
+    if soup:
+        for tag_a in soup.find_all('a', href=True):
+            link = tag_a.get('href')
+            link = absolute_link(link, host, domain)
+            if urlparse(link).netloc == domain and check_link_to_replay(link):
+                pages_append(link, 'No title', 'No description')
+                if max_depth != 0:
+                    queue.put(link)
+
+    if max_depth != 0:
+        while queue.qsize() >= threads_count:
+            thread_limit()
+            for i in range(threads_count):
+                target_link = queue.get_nowait()
+                thread_start(target=add_all_links_recursive, args=(target_link, host, domain, threads_count, 0))
+        while queue.qsize():
+            thread_limit()
+            target_link = queue.get_nowait()
+            thread_start(target=add_all_links_recursive, args=(target_link, host, domain, threads_count, 0))
+
+
+def add_title_description(soup, page):
+    title = soup.find('title')
+    if title:
+        page['title'] = title.get_text()
+    description = soup.find('meta', {'name': 'description'})
+    if description:
+        page['description'] = description['content']
 
 
 def add_page_info(page, host, domain):
-    html = get_html(page['url'])
-    if html:
-        try:
-            soup = BeautifulSoup(html.text, 'html.parser')
-            title = soup.find('title')
-            if title:
-                page['title'] = title.get_text()
-            description = soup.find('meta', {'name': 'description'})
-            if description:
-                page['description'] = description['content']
-            for tag_a in soup.find_all('a', href=True):
-                link = tag_a.get('href')
-                if all(not link.startswith(prefix) for prefix in FORBIDDEN_PREFIXES):
-                    if link.startswith('/') and not link.startswith('//'):
-                        link = host + domain + link
-                    if urlparse(link).netloc == domain:
-                        for element in Pages:
-                            if element['url'] == link:
-                                break
-                        try:
-                            link_sentence = tag_a.get_text()
-                            link_words = link_sentence.split()
-                            for word in link_words:
-                                flag = 0
-                                for letter in word:
-                                    if letter not in l_allowed:
-                                        flag = 1
-                                        break
-                                if flag == 0:
-                                    word = morph.parse(word)[0].normal_form
-                                    if word not in en_stops:
-                                        Words.append({
-                                            'id': Word_id[0],
-                                            'word': word,
-                                        })
-                                        Page_Word.append({
-                                            'page id': page['id'],
-                                            'word id': Word_id[0],
-                                        })
-                                        Link_Word_Page.append({
-                                            'page id': element['id'],
-                                            'word id': Word_id[0],
-                                        })
-                                        flag = 0
-                                        for d_word in Domain_quantity:
-                                            if word == d_word['word']:
-                                                d_word['quantity'] += 1
-                                                flag = 1
-                                        if flag == 0:
-                                            Domain_quantity.append({
-                                                'page id': domain,
-                                                'word': word,
-                                                'quantity': 1,
-                                            })
-                                        flag = 0
-                                        for p_word in Page_quantity:
-                                            if page['url'] == p_word['page id'] and word == p_word['word']:
-                                                p_word['quantity'] += 1
-                                                flag = 1
-                                        if flag == 0:
-                                            Page_quantity.append({
-                                                'page id': page['url'],
-                                                'word': word,
-                                                'quantity': 1,
-                                            })
-                                        Word_id.insert(0, Word_id[0] + 1)
-                        except: pass
-                tag_a.decompose()
+    soup = get_html(page['url'])
+    if soup:
+        add_title_description(soup, page)
+        for tag_a in soup.find_all('a', href=True):
+            link = tag_a.get('href')
+            link = absolute_link(link, host, domain)
+            if urlparse(link).netloc == domain and check_link_to_replay(link):
+                link_page_id = 0
+                for element in Pages:
+                    if element['url'] == link:
+                        link_page_id = element['id']
+                        break
+                link_sentence = tag_a.get_text()
+                link_words = link_sentence.split()
+                for word in link_words:
+                    word = words_append(word, page, domain)
+                    if word:
+                        page_word_append(page['id'], Word_id[0])
+                        link_word_page_append(link_page_id, Word_id[0])
+            tag_a.decompose()
 
-            body = soup.find('body')
-            if body:
-                text = body.get_text()
-                words = text.split()
-                for word in words:
-                    flag = 0
-                    for letter in word:
-                        if letter not in l_allowed:
-                            flag = 1
-                            break
-                    if flag == 0:
-                        word = morph.parse(word)[0].normal_form
-                        if word not in en_stops:
-                            Words.append({
-                                'id': Word_id[0],
-                                'word': word,
-                            })
-                            Page_Word.append({
-                                'page id': page['id'],
-                                'word id': Word_id[0],
-                            })
-                            flag = 0
-                            for d_word in Domain_quantity:
-                                if word == d_word['word']:
-                                    d_word['quantity'] += 1
-                                    flag = 1
-                            if flag == 0:
-                                Domain_quantity.append({
-                                    'page id': domain,
-                                    'word': word,
-                                    'quantity': 1,
-                                })
-                            flag = 0
-                            for p_word in Page_quantity:
-                                if page['url'] == p_word['page id'] and word == p_word['word']:
-                                    p_word['quantity'] += 1
-                                    flag = 1
-                            if flag == 0:
-                                Page_quantity.append({
-                                    'page id': page['url'],
-                                    'word': word,
-                                    'quantity': 1,
-                                })
-                            Word_id.insert(0, Word_id[0] + 1)
-        except: pass
+        body = soup.find('body')
+        if body:
+            text = body.get_text()
+            words = text.split()
+            for word in words:
+                flag = 0
+                for letter in word:
+                    if letter not in l_allowed:
+                        flag = 1
+                        break
+                if flag == 0:
+                    word = morph.parse(word)[0].normal_form
+                    if word not in en_stops:
+                        word = words_append(word, page, domain)
+                        if word:
+                            page_word_append(page['id'], Word_id[0])
 
 
 def save_file(path):
@@ -230,63 +241,62 @@ def save_file(path):
         save_file(file)
 
 
-def parse():
+def get_user_info():
     url = input('Введите url сайта: ')
     deep = input('Введите глубину просмотра сайта: ')
     file = input('Введите путь к файлу: ')
-    url.strip()
-    url_split = url.split('/')
-    host = url_split[0] + '//'
-    domain = url_split[2]
+    threads_count = int(input('Введите количество потоков обработки сайта: '))
+    user_info = {
+        'url': url.strip(),
+        'host': url.split('/')[0] + '//',
+        'domain': url.split('/')[2],
+        'deep': deep,
+        'file': file,
+        'threads_count': int(threads_count),
+    }
+    return user_info
 
-    if deep == '0':
+
+def main():
+    user_info = get_user_info()
+
+    if user_info['deep'] == '0':
         print('please wait...')
-        Pages.append({
-            'id': Page_id[0],
-            'url': url,
-            'title': 'No title',
-            'description': 'No description',
-        })
-        Page_id.insert(0, Page_id[0] + 1)
-        add_all_links_recursive(url, host, domain, max_depth=0)
+        pages_append(user_info['url'], 'No title', 'No description')
+        add_all_links_recursive(user_info['url'], user_info['host'], user_info['domain'], max_depth=0)
         print(f'Получено {len(Pages)} страниц')
-        print(f'Обработка {Pages[0]["url"]} 1 страницы из 1')
-        add_page_info(Pages[0], host, domain)
+
+        print(f'Обработка {user_info["url"]} 1 страницы из 1')
+        add_page_info(Pages[0], user_info['host'], user_info['domain'])
 
     else:
-        threads_count = int(input('Введите количество потоков обработки сайта: '))
         print('please wait...')
-        Pages.append({
-            'id': Page_id[0],
-            'url': host + domain + '/',
-            'title': 'No title',
-            'description': 'No description',
-        })
-        Page_id.insert(0, Page_id[0] + 1)
-        add_all_links_recursive(host + domain, host, domain)
+        pages_append(user_info['host'] + user_info['domain'] + '/', 'No title', 'No description')
+        add_all_links_recursive(user_info['host'] + user_info['domain'], user_info['host'], user_info['domain'], user_info['threads_count'], max_depth=1)
+        for t in Thread_list:
+            t.join()
+        Thread_list.clear()
         print(f'Получено {len(Pages)} страниц')
-        if threads_count > len(Pages):
-            threads_count = input(f'Число потоков должно быть меньше {len(Pages)}: ')
+
+        if user_info['threads_count'] > len(Pages):
+            user_info['threads_count'] = input(f'Введите число потоков меньшее {len(Pages)}: ')
+
         print('Запуск потоков...')
-        thread_list = []
-        for page in range(0, len(Pages), threads_count):
-            if (threads_count + page) < len(Pages):
-                for i in range(threads_count):
-                    t = threading.Thread(target=add_page_info, args=(Pages[page + i], host, domain))
-                    thread_list.append(t)
-                    print(f'обработка {Pages[page + i]["url"]} {Pages[page + i]["id"]} страница из {len(Pages)}')
-                    t.start()
-                for t in thread_list:
-                    t.join()
-                thread_list.clear()
-        remaining = len(Pages) - (len(Pages) % threads_count)
+        for pages in range(0, len(Pages), user_info['threads_count']):
+            if (user_info['threads_count'] + pages) <= len(Pages):
+                for i in range(user_info['threads_count']):
+                    print(f'обработка {Pages[pages + i]["url"]} {Pages[pages + i]["id"]} страница из {len(Pages)}')
+                    thread_start(target=add_page_info, args=(Pages[pages + i], user_info['host'], user_info['domain']))
+                thread_limit()
+
+        remaining = len(Pages) - (len(Pages) % user_info['threads_count'])
         for i in range(remaining, len(Pages)):
             print(f'обработка {Pages[i]["url"]} {Pages[i]["id"]} страница из {len(Pages)}')
-            add_page_info(Pages[i], host, domain)
+            add_page_info(Pages[i], user_info['host'], user_info['domain'])
 
-    save_file(file)
+    save_file(user_info['file'])
 
     print('success')
 
 
-parse()
+main()
